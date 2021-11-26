078da362708eefa20b144d194aa5ab3217afa7d6
Kurnia <92375799+KurniaRamadhani11@users.noreply.github.com>
2021-11-26 04:07:03 -0800

    Add files via upload

---
+++ 2117051073_KurniaRamadhani_Postest1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	// Kurnia Ramadhani 2117051073 //
+	
+	//string//
+	string variabel_nama1;
+	string variabel_nama2;
+	
+	//input//
+	getline(cin,variabel_nama1);
+	getline(cin,variabel_nama2);
+	cout<<"                 "<<endl;
+	//output//
+	cout
+	<<"::::::::::Percakapan Singkat::::::::::"<<endl
+	<<" "<<variabel_nama1<<"    :Hai salam kenal namaku \""<<variabel_nama1<<"\",nama kamu siapa? "<<endl
+	<<" "<<variabel_nama2<<"    :Hai salam kenal juga namaku \""<<variabel_nama2<<"\" "<<endl
+	<<"::::::::::::::::::::::::::::::::::::::"<<endl;
+	return 0;
+}
+
